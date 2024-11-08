93e32dc2d9882e8f60313807b318303c367f9a06
LucjaTalaga <lucja.talaga@gmail.com>
2024-11-08 13:24:02 +0100

    adding docker and basic server config

--- app/server.js
+++ app/server.js
@@ -1,1 +1,23 @@
 let express = require('express');
+let path = require('path');
+let fs = require('fs');
+let MongoClient = require('mongodb').MongoClient;
+let bodyParser = require('body-parser');
+let app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+
+
+
+app.listen(3001, function () {
+    console.log("app listening on port 3001!");
+});
+  
