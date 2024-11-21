58a667ee1b6d84d1911a99f0cc888d4069d188f9
LucjaTalaga <lucja.talaga@gmail.com>
2024-11-21 22:10:12 +0100

    adding mongo config

--- app/script.js
+++ app/script.js
@@ -1,1 +1,9 @@
-// here will be frontend
+console.log("script is up");
+
+(async function init(){
+    console.log("Init runs");
+    const response = await fetch('http://localhost:3001/get-cats');
+    console.log("response", response);
+    const cats = await response.json();
+    console.log(JSON.stringify(cats));
+})();
--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 let express = require('express');
 let path = require('path');
 let fs = require('fs');
+let cors = require('cors');
 let MongoClient = require('mongodb').MongoClient;
 let bodyParser = require('body-parser');
 let app = express();
@@ -14,9 +15,33 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
+app.use(express.static(path.join(__dirname, "/")));
+app.use(cors());
+
+
+
 //mongo client options and credentials
-let mongoUrl = "mongodb://admin:pass@mongodb";
+let mongoUrl = "mongodb://admin:password@localhost:27017";
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
+let dbName = "my-db";
+
+app.get('/get-cats', function (req, res) {
+  let response = {};
+  // Connect to the db
+  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
+    if (err) throw err;
+    else {
+      console.log("mongo connection estabilished");
+    }
+
+    let db = client.db(dbName);
+    // Send response
+    res.send(response ? response : {});
+
+
+
+  });
+});
 
 
 
